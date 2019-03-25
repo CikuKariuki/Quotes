89fa9b688362b810563d16ee6e926ab568d5429a
Wanjiku <Wanjiku>
2019-03-25 12:35:48 +0300

    added custom pipe, date counter

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,12 @@
 export class Quote {
   public showDescription:boolean;
-  constructor(public id:number, public name:string, public author:string,public submitter:string, public upVote:number, public downVote:number){
+  constructor(public id:number, 
+    public name:string, 
+    public author:string,
+    public submitter:string,
+    public upVote:number,
+    public downVote:number,
+    public completeDate:Date){
     this.showDescription=false
   }
 
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -7,23 +7,23 @@ import { Quote } from '../quote'
 })
 export class QuoteComponent implements OnInit {
   quotes= [
-    new Quote(1, "You don't have to see the whole staircase, just take the first step. ","Martin Luther King","",0,0),
-    new Quote(2, "A smooth sea never made a skilled sailor.","English Proverb","",0,0),
-    new Quote(3, "Learn from yesterday, live for today, hope for tomorrow.","Albert Einstein","",0,0),
-    new Quote(4, "The best and most beautiful things in the world cannot be seen or touched. They must be felt with the heart.","Helen Keller","",0,0),
-    new Quote(5, "Great thoughts speak only to the thoughtful mind, but great actions speak to all  mankind.","Emily P. Bissell","",0,0),
-    new Quote(6, "We don't see things as they are, we see things as we are.", "Anais Nin","",0,0),
-    new Quote(7, "Kind words can be short and easy to speak, but their echoes are truly endless.", "Mother Teresa","",0,0),
-    new Quote(8, "We are not rich by what we possess rather by what we can do without.", "Immanuel Kant","",0,0),
-    new Quote(9, "Most folks are about as happy as they make up their minds to be.", "Abraham Lincoln","",0,0),
-    new Quote(10, "You don't get harmony when everybody sings the same note.", "Doug Floyd","",0,0)
+    new Quote(1, "You don't have to see the whole staircase, just take the first step. ","Author: Martin Luther King","",0,0,new Date(2019,2,24)),
+    new Quote(2, "A smooth sea never made a skilled sailor.","Author: English Proverb","",0,0,new Date(2019,2,24)),
+    new Quote(3, "Learn from yesterday, live for today, hope for tomorrow.","Author: Albert Einstein","",0,0,new Date(2019,2,24)),
+    new Quote(4, "The best and most beautiful things in the world cannot be seen or touched. They must be felt with the heart.","Author: Helen Keller","",0,0,new Date(2019,2,24)),
+    new Quote(5, "Great thoughts speak only to the thoughtful mind, but great actions speak to all  mankind.","Author: Emily P. Bissell","",0,0,new Date(2019,2,24)),
+    new Quote(6, "We don't see things as they are, we see things as we are.", "Author: Anais Nin","",0,0,new Date(2019,2,24)),
+    new Quote(7, "Kind words can be short and easy to speak, but their echoes are truly endless.", "Author: Mother Teresa","",0,0,new Date(2019,2,24)),
+    new Quote(8, "We are not rich by what we possess rather by what we can do without.", "Author: Immanuel Kant","",0,0,new Date(2019,2,24)),
+    new Quote(9, "Most folks are about as happy as they make up their minds to be.", "Author: Abraham Lincoln","",0,0,new Date(2019,2,24)),
+    new Quote(10, "You don't get harmony when everybody sings the same note.", "Author: Doug Floyd","",0,0,new Date(2019,2,24))
   ]
   toggleDetails(index){
     this.quotes[index].showDescription= !this.quotes[index].showDescription;
   }
   deleteQuote(isComplete,index){
     if(isComplete){
-      let toDelete=confirm(`Are you sure you want to delete ${this.quotes[index].name}`)
+      let toDelete=confirm('Are you sure you want to delete this quote?')
 
       if(toDelete){
         this.quotes.splice(index,1);
